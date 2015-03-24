3f787ee986da099380c58c0cabc60423b0e3cf39
Mr. John E! <jonki90@gmail.com>
2015-03-24 11:43:11 +0000

    test

--- app/src/main/java/com/example/nammi/bilapower/Bilapower.java
+++ app/src/main/java/com/example/nammi/bilapower/Bilapower.java
@@ -12,7 +12,6 @@ public class Bilapower extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bilapower);
-        hello?
     }
 
 
